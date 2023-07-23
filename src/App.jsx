77db76d134844ecb22a368ced656c872a85f5814
Tanishka <makodetanishka@gmail.com>
2023-07-23 11:33:32 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import PopUp from "./components/PopUp";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Nav />
+      <Header />
+      <Feed />
+      <PopUp />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/PopUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PopUp = () => {
+  return <div className="popup"></div>;
+};
+
+export default PopUp;
